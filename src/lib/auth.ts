ea3627765c4f5fd2d1ec6d4f0dad93f6b4c5123a
Adwaidh <adwaidhcke2@gmail.com>
2025-05-31 02:27:24 +0530

    changed logic in nextAuth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,7 +5,14 @@ import { compare } from "bcrypt";
 import { NextAuthOptions, Session } from "next-auth";
 import { JWT } from "next-auth/jwt";
 
-const prisma = new PrismaClient();
+// Prevent multiple instances of Prisma Client in development
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
